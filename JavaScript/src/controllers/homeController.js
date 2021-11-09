ef613259059e50b3f09ff1c4c58d7cf1532c2f67
Nguyễn Văn Quang <Quang.nv193069@sis.hust.edu.vn>
2021-11-09 23:07:03 +0700

    first commit

---
+++ JavaScript/src/controllers/homeController.js
@@ -0,0 +1,75 @@
+import db from "../models/index";
+import CRUDservice from "../services/CRUDservice";
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('homepage.ejs', { data: JSON.stringify(data) });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+let getAboutPage = (req, res) => {
+    return res.render('about.ejs');
+}
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+let postCRUD = async (req, res) => {
+    let message = await CRUDservice.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud');
+}
+let displayCRUD = async (req, res) => {
+    let data = await CRUDservice.getAllUser();
+    console.log(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    });
+}
+let editCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDservice.getUserInfoById(userId);
+        return res.render('edit_CRUD.ejs', {
+            user: userData
+        });
+    }
+    else {
+        return res.send('user not found!');
+    }
+}
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDservice.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    })
+}
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        let data = req.body;
+        let allUsers = await CRUDservice.deleteCRUD(id);
+        return res.render('displayCRUD.ejs', {
+            dataTable: allUsers
+        })
+    }
+    else {
+        res.send('user not found!');
+    }
+}
+// object: {
+//     key: '',
+//     value: ''
+// }
+module.exports = {
+    getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayCRUD: displayCRUD,
+    editCRUD: editCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
+}
