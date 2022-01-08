ecd5410aa76121d5dfe8ac39fcc186ac2a38c1b0
Nguyễn Văn Quang <Quang.nv193069@sis.hust.edu.vn>
2022-01-08 21:09:17 +0700

    video 61

--- JavaScript/src/services/doctorService.js
+++ JavaScript/src/services/doctorService.js
@@ -157,14 +157,9 @@ let bulkCreateSchedule = (data) => {
                         raw: true
                     }
                 );
-                if (existing && existing.length > 0) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime();
-                        return item;
-                    })
-                }
+
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
                 if (toCreate && toCreate.length > 0) {
                     await db.Schedule.bulkCreate(toCreate);
@@ -193,7 +188,12 @@ let getScheduleByDate = (doctorId, date) => {
                     where: {
                         doctorId: doctorId,
                         date: date
-                    }
+                    },
+                    include: [
+                        { model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi'] },
+                    ],
+                    raw: false,
+                    nest: true
                 })
                 if (!dataSchedule) dataSchedule = [];
                 resolve({
